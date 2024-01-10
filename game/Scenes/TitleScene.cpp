c13bbe7bb93536fa7fa5e367f6ca3fb872250ff6
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2024-01-10 14:13:53 +0900

    1_5完成

--- game/Scenes/TitleScene.cpp
+++ game/Scenes/TitleScene.cpp
@@ -8,34 +8,42 @@ void TitleScene::Initialize()
 {	
 	input = Input::GetInstance();
 	count = 0;
-	pointY = { 2.1f,-0.9f,1.3f };
+	
 }
 
 void TitleScene::Update()
 {
-	Rotation = createQuaternion(0.45f,Normalise({ 1.0f,0.4f,-0.2f }));
-	
+	Rotation = createQuaternion(0.3f,Normalise({ 0.71f,0.71f,0.0f }));
+	rotation1 = createQuaternion(3.141592f, { 0.71f,0.0f,0.71f });
+	interpolate[0] = Slerp(0.0f, Rotation, rotation1);
+	interpolate[1] = Slerp(0.3f, Rotation, rotation1);
+	interpolate[2] = Slerp(0.5f, Rotation, rotation1);
+	interpolate[3] = Slerp(0.7f, Rotation, rotation1);
+	interpolate[4] = Slerp(1.0f, Rotation, rotation1);
 	Vector4 rot = QuaternionToVEctor4(Rotation);
-	rotateMatrix= quaternionToMatrix(Normalize( Rotation));
-	rotateByMatrix = vectorTransform(pointY, rotateMatrix);
-	rotateByQuaternion = Rotatevector(pointY, Rotation);
-	std::string result;
+	Vector4 inter[5];
 	ImGui::Begin("");
-	ImGui::InputFloat4("rotation",&rot.x );
-	ImGui::InputFloat3("rotateByMatrix", &rotateByMatrix.x);
-	ImGui::InputFloat3("rotateByQuaternion", &rotateByQuaternion.x);
-	
-	for (int y = 0; y < 4; y++) {
-		for (int x = 0; x < 4; x++) {
-			result += std::to_string(rotateMatrix.m[y][x]) + ", ";
-			
-		}
-		result += "\n";
-	
+	ImGui::InputFloat4("rotation", &rot.x);
+	for (int i = 0; i < 5; i++) {
+		inter[i] = QuaternionToVEctor4(interpolate[i]);
+		ImGui::InputFloat4("interpolate", &inter[i].x);
 	}
-	ImGui::Text("matrix\n");
-	ImGui::Text("%s", result.c_str());
 	ImGui::End();
+	//std::string result;
+	
+	
+	//
+	//for (int y = 0; y < 4; y++) {
+	//	for (int x = 0; x < 4; x++) {
+	//		result += std::to_string(rotateMatrix.m[y][x]) + ", ";
+	//		
+	//	}
+	//	result += "\n";
+	//
+	//}
+	//ImGui::Text("matrix\n");
+	//ImGui::Text("%s", result.c_str());
+	//ImGui::End();
 	if (input->PushKey(DIK_SPACE)) {
 		sceneNum = GAME_SCENE;
 		count++;
--- game/Scenes/TitleScene.h
+++ game/Scenes/TitleScene.h
@@ -17,10 +17,8 @@ public:
 private:
 	Input* input;
 	Quaternion Rotation;
-	Vector3 pointY = { 2.1f,-0.9f,1.3f };
-	Matrix4x4 rotateMatrix;
-	Vector3 rotateByQuaternion;
-	Vector3 rotateByMatrix;
+	Quaternion rotation1;
+	Quaternion interpolate[5];
 
 };
 
